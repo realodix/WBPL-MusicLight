b90227c0809c55fe184bdc04de13d29b87609831
Realodix <realodix@yahoo.com>
2013-03-20 10:56:11 +0700

    Add style.css, kode css mandiri.

--- logout.php
+++ logout.php
@@ -4,6 +4,8 @@
 	<title>Music Light</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
+	<link href="css/style.css" rel="stylesheet" media="screen">
+	
 	<!-- Bootstrap -->
 	<link href="css/bootstrap.css" rel="stylesheet" media="screen">
 	<link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen">
