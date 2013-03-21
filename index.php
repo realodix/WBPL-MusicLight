bbe054970738009296a6e85cc785cef14574f196
Realodix <realodix@yahoo.com>
2013-03-21 10:34:38 +0700

    Home: Menambah isi konten

--- index.php
+++ index.php
@@ -69,7 +69,31 @@
     </div>
 
 <div class="container">
-	<h1>Hello, world!</h1>
+
+	<h3>Welcome to Music Light</h3>
+
+	<form class="form-horizontal">
+		<div class="control-group">
+			<label class="control-label" for="inputUsername">Username</label>
+			<div class="controls">
+				<input type="text" id="inputUsername" placeholder="Username">
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label" for="inputPassword">Password</label>
+			<div class="controls">
+				<input type="password" id="inputPassword" placeholder="Password">
+			</div>
+		</div>
+	
+		<div class="control-group">
+			<div class="controls">
+				<button type="submit" class="btn">Sign in</button>
+			</div>
+		</div>
+    </form>
+	
 </div>
 </body>
 </html>
