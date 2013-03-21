f416b0c9951a717ce84a58473f349ec2b4d0e509
Realodix <realodix@yahoo.com>
2013-03-21 10:29:04 +0700

    Menambah navigasi

--- logout.php
+++ logout.php
@@ -12,6 +12,62 @@
 </head>
 
 <body>
+
+<!-- Navbar
+    ================================================== -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="./index.html">Music Light</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="">
+                <a href="./index.php">Home</a>
+              </li>
+              <li class="">
+                <a href="./registration.php">Registration</a>
+              </li>
+              <li class="">
+                <a href="./product.php">Product</a>
+              </li>
+              <li class="">
+                <a href="./insertproduct.php">Insert Product Page</a>
+              </li>
+              <li class="">
+                <a href="./updateproduct.php">Update Product</a>
+              </li>
+              <li class="">
+                <a href="./profile.php">Profile</a>
+              </li>
+              <li class="">
+                <a href="./member.php">Member</a>
+              </li>
+			  <li class="">
+                <a href="./cart.php">Cart</a>
+              </li>
+			  <li class="">
+                <a href="./transaction.php">Transaction</a>
+              </li>
+			  <li class="">
+                <a href="./detail.php">Detail</a>
+              </li>
+			  <li class="">
+                <a href="./testimony.php">Testimony</a>
+              </li>
+			  <li class="active">
+                <a href="./logout.php">Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
 <div class="container">
 	<h1>Hello, world!</h1>
 </div>
