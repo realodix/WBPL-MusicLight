14ccc10e7acff360bbf1568f9d1c783c0c7c33cc
Realodix <realodix@yahoo.com>
2013-03-21 09:49:29 +0700

    Reg: Menambah isi konten

--- registration.php
+++ registration.php
@@ -12,8 +12,80 @@
 </head>
 
 <body>
-<div class="container">
-	<h1>Hello, world!</h1>
+<div class="container register">
+	
+	<h3>Registration Page</h3>
+	
+	<form class="form-horizontal">
+		<div class="control-group">
+			<label class="control-label" for="inputName">Name</label>
+			<div class="controls">
+				<input type="text" id="inputName" placeholder="Name">
+			</div>
+		</div>
+	
+		<div class="control-group">
+			<label class="control-label" for="inputUsername">Username</label>
+			<div class="controls">
+				<input type="text" id="inputUsername" placeholder="Username">
+			</div>
+		</div>
+	
+		<div class="control-group">
+			<label class="control-label" for="inputEmail">Email</label>
+			<div class="controls">
+				<input type="text" id="inputEmail" placeholder="Email">
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label" for="inputPassword">Password</label>
+			<div class="controls">
+				<input type="password" id="inputPassword" placeholder="Password">
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label" for="inputCPassword">Confirm Password</label>
+			<div class="controls">
+				<input type="password" id="inputCPassword" placeholder="Confirm Password">
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label" for="inputGender">Gender</label>
+			<div class="controls">
+				<label class="radio">
+					<input type="radio" name="gender" id="male" value="option1" checked>Male
+				</label>
+				<label class="radio">
+					<input type="radio" name="gender" id="female" value="option2">Female
+				</label>
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label" for="inputAddress">Address</label>
+			<div class="controls">
+				<textarea rows="3"></textarea>
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label" for="inputPhone ">Phone </label>
+			<div class="controls">
+				<input type="text" id="inputPhone" placeholder="Phone ">
+			</div>
+		</div>
+	
+		<div class="control-group">
+			<div class="controls">
+				<input type="button" value="Submit" class="btn" onclick="Cek()" />
+				<input type="reset" value="Reset" class="btn" onclick="Reset()" />
+			</div>
+		</div>
+    </form>
+	
 </div>
 </body>
 </html>
