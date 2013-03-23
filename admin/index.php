d4ebe8b2f64354b8d8c80dd6af5b09217175159d
Realodix <realodix@yahoo.com>
2013-03-23 19:49:08 +0700

    Menambahkan halaman index.php pada admin.

---
+++ admin/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Administrator Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link href="../css/style.css" rel="stylesheet" media="screen">
+	
+	<!-- Bootstrap -->
+	<link href="../css/bootstrap.css" rel="stylesheet" media="screen">
+	<link href="../css/bootstrap-responsive.css" rel="stylesheet" media="screen">
+</head>
+
+<body>
+
+<!-- Navbar
+    ================================================== -->
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="./index.html">Music Light</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="">
+                <a href="./index.php">Home</a>
+              </li>
+              <li class="">
+                <a href="./product.php">Product</a>
+              </li>
+              <li class="">
+                <a href="./insertproduct.php">Insert Product</a>
+              </li>
+              <li class="">
+                <a href="./updateproduct.php">Update Product</a>
+              </li>
+              <li class="">
+                <a href="./profile.php">Profile</a>
+              </li>
+              <li class="">
+                <a href="./member.php">Member</a>
+              </li>
+
+			  <li class="">
+                <a href="./transaction.php">Transaction</a>
+              </li>
+			  <li class="active">
+                <a href="">Detail</a>
+              </li>
+			  <li class="">
+                <a href="./logout.php">Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<div class="container">
+
+    <div class="row">
+    <div class="span6">
+	<h3>Welcome to Music Light</h3>
+	
+	<p>Music Light is a famous musical instrument shop in the town. It sells a wide range of musical instruments from the common to the rare. As time passed, the owner of this store realized how important the internet today and decided to develop a website that can ease customer to make transactions in Music Light. This website will be used by the customer/member to order musical instruments via online and promote Music Light’s product to non-member visitor.</p>
+	
+	</div>
+	
+    <div class="span3">
+		
+		<h4>Login</h4>
+		
+		<form class="form-horizontal">
+		<div class="control-group">
+			<label class="control-label" for="inputUsername">Username</label>
+			<div class="controls">
+				<input type="text" id="inputUsername" placeholder="Username">
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label" for="inputPassword">Password</label>
+			<div class="controls">
+				<input type="password" id="inputPassword" placeholder="Password">
+			</div>
+		</div>
+	
+		<div class="control-group">
+			<div class="controls">
+				<button type="submit" class="btn">Sign in</button>
+			</div>
+		</div>
+    </form>
+	</div>
+    </div>
+
+	
+</div>
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap-transition.js"></script>
+    <script src="../js/bootstrap-alert.js"></script>
+    <script src="../js/bootstrap-modal.js"></script>
+    <script src="../js/bootstrap-dropdown.js"></script>
+    <script src="../js/bootstrap-scrollspy.js"></script>
+    <script src="../js/bootstrap-tab.js"></script>
+    <script src="../js/bootstrap-tooltip.js"></script>
+    <script src="../js/bootstrap-popover.js"></script>
+    <script src="../js/bootstrap-button.js"></script>
+    <script src="../js/bootstrap-collapse.js"></script>
+    <script src="../js/bootstrap-carousel.js"></script>
+    <script src="../js/bootstrap-typeahead.js"></script>
+    <script src="../js/bootstrap-affix.js"></script>
+
+    <script src="../js/application.js"></script>
+
+</body>
+</html>
