c2c9bb1c7b94c1538d29277b1c5ac224418e2ebe
Realodix <realodix@yahoo.com>
2013-03-31 15:39:30 +0700

    #19 Pesan: Menambahkan huruf 'z'

--- admin/index.php
+++ admin/index.php
@@ -83,7 +83,7 @@ require_once('../inc/common_function.php');
          <h2>Pesan</h2>
 		  <ul  class="arrow">
 		  <li><a href="index.php?page=customer_view">Customer</a></li>
-		  <li><a href="index.php?page=pesan_view">Pesan</a></li>
+		  <li><a href="index.php?page=zpesan_view">Pesan</a></li>
 		  <li><a href="index.php?page=det_pesan_view">Detail pemesanan</a></li>
 		  <li><a href="index.php?page=zbayar_view">pembayaran</a></li>
 		  </ul>
