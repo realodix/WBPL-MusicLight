4578a1f7778d1e90bddd3ea2a84fe000a3a51b9f
Realodix <realodix@yahoo.com>
2013-04-01 16:40:06 +0700

    Fix Undefined index pada halaman finish.php

--- finish.php
+++ finish.php
@@ -1,13 +1,16 @@
 <?
 
-$id_kota = $_SESSION['id_kota'];
-$kd_pesan=$_SESSION['kd_pesan'];
+//$id_kota = $_SESSION['id_kota'];
+$id_kota = isset($_SESSION['id_kota'])?$_SESSION['id_kota']:false;
+//$kd_pesan=$_SESSION['kd_pesan'];
+$kd_pesan = isset($_SESSION['kd_pesan'])?$_SESSION['kd_pesan']:false;
 //echo "nama kota:".$id_kota;
 //echo "SESSION KOTA:".$_SESSION['id_kota'];
 $get_data = mysql_query("select biaya from biaya_kirim where id_kota='$id_kota'") or die(mysql_error());
 $biaya = mysql_fetch_array($get_data);
 $biaya = $biaya['biaya'];
-$totalBayar = $_SESSION['totalbayar'];
+//$totalBayar = $_SESSION['totalbayar'];
+$totalBayar = isset($_SESSION['totalBayar'])?$_SESSION['totalBayar']:false;
 ?>
 
 <h3> Selamat,Transaksi sukses di lakukan</h2>
