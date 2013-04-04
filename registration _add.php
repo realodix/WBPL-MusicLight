94eb420d0747bea70d97a220ff977834afe15c98
Realodix <realodix@yahoo.com>
2013-04-04 17:10:44 +0700

    #2 mengambil data dri user.

--- registration _add.php
+++ registration _add.php
@@ -1,3 +1,28 @@
 <?php
 include ('admin/inc/config.php');
+
+//data dari user
+if (isset($_POST['register_user'])) {
+	$name_user = $_POST['name_user'];
+	$username_user = $_POST['username_user'];
+	$pass_user = $_POST['pass_user'];
+	$cpass_user = $_POST['cpass_user'];
+	/*$gender_user = $_POST['gender_user'];*/
+	$address_user = $_POST['address_user'];
+	$phone_user = $_POST['phone_user'];
+	$email_user = $_POST['email_user'];
+
+
+	/*$sql = "INSERT INTO wbpl_brand(kd_brand,nama_brand)
+		VALUES('$kd_brand', '$nama_brand')";
+	$result = mysql_query($sql) or die(mysql_error());*/
+
+	//check if query successful
+	if ($result) {
+		header('location:index.php?page=brand_view&status=0');
+	} else {
+		header('location:index.php?page=brand_view&status=1');
+	}
+	mysql_close();
+}
 ?>
