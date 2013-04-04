0e314f0d95e29fdebfd7270cd3862be442938ffa
Realodix <realodix@yahoo.com>
2013-04-04 15:42:12 +0700

    #2 Add registration form

--- registration.php
+++ registration.php
@@ -1,2 +1,75 @@
 <h1>Registration</h2>
-<p> Halaman registrasi </p>
+
+<form id="registration" name="registration" method="post" action="">
+<table>
+	<tr>
+		<td>Name</td>
+		<td></td>
+		<td></td>
+		<td><input type="text"></input></td>
+	</tr>
+	
+	<tr>
+		<td>Username</td>
+		<td></td>
+		<td></td>
+		<td><input type="text"></input></td>
+	</tr>
+	
+	<tr>
+		<td>Password</td>
+		<td></td>
+		<td></td>
+		<td><input type="text"></input></td>
+	</tr>
+	
+	<tr>
+		<td>Confirm Password</td>
+		<td></td>
+		<td></td>
+		<td><input type="text"></input></td>
+	</tr>
+	
+	<tr>
+		<td>Gender</td>
+		<td></td>
+		<td></td>
+		<td><input type="text"></input></td>
+	</tr>
+	
+	<tr>
+		<td>Address</td>
+		<td></td>
+		<td></td>
+		<td><input type="text"></input></td>
+	</tr>
+	
+	<tr>
+		<td>Phone</td>
+		<td></td>
+		<td></td>
+		<td><input type="text"></input></td>
+	</tr>
+	
+	<tr>
+		<td>E-Mail</td>
+		<td></td>
+		<td></td>
+		<td><input type="text"></input></td>
+	</tr>
+	
+	<tr>
+		<td>E-Mail</td>
+		<td></td>
+		<td></td>
+		<td><input type="text"></input></td>
+	</tr>
+	
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td><input type="submit" name="register_user" value="Register" /></td>
+	</tr>
+</table>
+</form>
