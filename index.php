4b2538d1b89d95805b3188a47b80f0da9aa05b8d
Realodix <realodix@yahoo.com>
2013-04-05 11:55:26 +0700

    Artikel di halaman home, tidak lagi menggunkan include.

--- index.php
+++ index.php
@@ -169,11 +169,11 @@
                 <?php
 				/* kode untuk meload halaman yang berbeda*/
 				if(isset($_GET['page'])) {
-				$page = $_GET['page'] . ".php";
-				include ($page);
-
+					$page = $_GET['page'] . ".php";
+					include ($page);
 				} else {
-					include ('home.php');
+					echo '<h3>Welcome to Music Light</h3>
+					<P>iMusic Light is a famous musical instrument shop in the town. It sells a wide range of musical instruments from the common to the rare. As time passed, the owner of this store realized how important the internet today and decided to develop a website that can ease customer to make transactions in Music Light. This website will be used by the customer/member to order musical instruments via online and promote Music Light’s product to non-member visitor.</p>';
 				}?>
                 	
             </div>
