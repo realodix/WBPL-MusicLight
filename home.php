6a3c39473fb0de019b99fbd1ffbed6defb473b52
Realodix <realodix@yahoo.com>
2013-04-05 18:40:09 +0700

    Membenahi penulisan artikel sesuai soal.

--- home.php
+++ home.php
@@ -174,7 +174,7 @@
 					include ($page);
 				} else {
 					echo '<h3>Welcome to Music Light</h3>
-					<P>iMusic Light is a famous musical instrument shop in the town. It sells a wide range of musical instruments from the common to the rare. As time passed, the owner of this store realized how important the internet today and decided to develop a website that can ease customer to make transactions in Music Light. This website will be used by the customer/member to order musical instruments via online and promote Music Light’s product to non-member visitor.</p>';
+					<P>Music Light is a famous musical instrument shop in the town. It sells a wide range of musical instruments from the common to the rare. As time passed, the owner of this store realized how important the internet today and decided to develop a website that can ease customer to make transactions in Music Light. This website will be used by the customer/member to order musical instruments via online and promote Music Light’s product to non-member visitor.</p>';
 				}?>
                 	
             </div>
