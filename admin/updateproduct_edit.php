9d63ec76513fc5ec7f3e49ed9048a036b900b34b
Realodix <realodix@yahoo.com>
2013-04-07 23:30:20 +0700

    Untuk sementara, update brand & instrument ditiadakan.

--- admin/updateproduct_edit.php
+++ admin/updateproduct_edit.php
@@ -3,9 +3,10 @@ include ('inc/config.php');
 //data dari user
 if (isset($_POST['tambah'])) {
 
-	$kd_brand = $_POST['kd_brand'];
-	$kd_instype = $_POST['kd_instype'];
+	//$kd_brand = $_POST['kd_brand'];
+	//$kd_instype = $_POST['kd_instype'];
 	$product_price = $_POST['product_price'];
+	$product_stock = $_POST['product_stock'];
 	$product_deskripsi = $_POST['product_deskripsi'];
 
 	$lokasi_file = $_FILES['product_image']['tmp_name'];
@@ -16,13 +17,11 @@ if (isset($_POST['tambah'])) {
 if (move_uploaded_file($lokasi_file, $direktori)) {
 				
 	$sql = "UPDATE wbpl_product SET
-		kd_brand='$kd_brand',
-		kd_instype='$kd_instype',
 		product_price='$product_price',
+		product_stock='$product_stock',
 		product_image='$product_image',
-		product_deskripsi='$product_deskripsi'
-		where wbpl_product.kd_brand = wbpl_brand.kd_brand AND
-				wbpl_product.kd_instype = wbpl_instype.kd_instype";
+		product_deskripsi='$product_deskripsi'";
+
 	
 	$result = mysql_query($sql) or die(mysql_error());
 
