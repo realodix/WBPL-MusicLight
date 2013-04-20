8ccb9bf2d16424106d04c14c97b9dbc37aad71bb
Realodix <realodix@yahoo.com>
2013-04-21 00:41:36 +0700

    Menyusun penulisan kode agar lebih mudah untuk dibaca.

--- cart.php
+++ cart.php
@@ -12,46 +12,26 @@ session_start();
 
 // Process actions
 if(isset($_GET['action'])){
-$cart = $_SESSION['cart'];
-if(isset($_GET['action'])){
-	$action = $_GET['action'];
+	$cart = $_SESSION['cart'];
+	if(isset($_GET['action'])){
+		$action = $_GET['action'];
 
-switch ($action) {
-	case 'add' :
-		if ($cart) {
-			$cart .= ',' . $_GET['id'];
-		} else {
-			$cart = $_GET['id'];
-		}
-		break;
-		//
-		//B002,5,B003,10
-	case 'delete' :
-		if ($cart) {
-			$items = explode(',', $cart);
-			$newcart = '';
-			foreach ($items as $item) {
-				if ($_GET['id'] != $item) {
-					if ($newcart != '') {
-						$newcart .= ',' . $item;
-					} else {
-						$newcart = $item;
-					}
+		switch ($action) {
+			case 'add' :
+				if ($cart) {
+					$cart .= ',' . $_GET['id'];
+				} else {
+					$cart = $_GET['id'];
 				}
-			}
-			$cart = $newcart;
-		}
-		break;
-	case 'update' :
-		if ($cart) {
-			$newcart = '';
-			foreach ($_POST as $key => $value) {
-				if (stristr($key, 'qty')) {
-					$id = str_replace('qty', '', $key);
-					$items = ($newcart != '') ? explode(',', $newcart) : explode(',', $cart);
+			break;
+				//
+				//B002,5,B003,10
+			case 'delete' :
+				if ($cart) {
+					$items = explode(',', $cart);
 					$newcart = '';
 					foreach ($items as $item) {
-						if ($id != $item) {
+						if ($_GET['id'] != $item) {
 							if ($newcart != '') {
 								$newcart .= ',' . $item;
 							} else {
@@ -59,22 +39,43 @@ switch ($action) {
 							}
 						}
 					}
-					for ($i = 1; $i <= $value; $i++) {
-						if ($newcart != '') {
-							$newcart .= ',' . $id;
-						} else {
-							$newcart = $id;
+					$cart = $newcart;
+				}
+			break;
+			
+			case 'update' :
+				if ($cart) {
+					$newcart = '';
+					foreach ($_POST as $key => $value) {
+						if (stristr($key, 'qty')) {
+							$id = str_replace('qty', '', $key);
+							$items = ($newcart != '') ? explode(',', $newcart) : explode(',', $cart);
+							$newcart = '';
+							foreach ($items as $item) {
+								if ($id != $item) {
+									if ($newcart != '') {
+										$newcart .= ',' . $item;
+									} else {
+										$newcart = $item;
+									}
+								}
+							}
+							for ($i = 1; $i <= $value; $i++) {
+								if ($newcart != '') {
+									$newcart .= ',' . $id;
+								} else {
+									$newcart = $id;
+								}
+							}
 						}
 					}
 				}
-			}
-		}
 
-		$cart = $newcart;
-		break;
-}
-}
-$_SESSION['cart'] = $cart;
+				$cart = $newcart;
+			break;
+		}
+	}
+	$_SESSION['cart'] = $cart;
 }
 ?>
 
@@ -84,101 +85,84 @@ $_SESSION['cart'] = $cart;
 	echo writeShoppingCart();
 	?>
 </div>
+
 <div id="contents">
 	<h3>Cek keranjang belanja</h3>
 	<?php
-	//echo showCart();
+	//echo wbpl_showCart();
 	echo wbpl_showCart();
 	?>
 
 	<h3>Form Pengiriman barang</h3>
 	<a href="index.php?page=cart&action=finish&kirim=true">Isi Data pembeli</a>
+	
 	<?php
-if(isset($_GET['kirim'])){
-	?>
-	<form id="form1" name="form1" method="post" action="pemesan_add.php">
-		<td>
-		<table>
-			<tr>
-				<td width="120"><b>Nama</b></td>
-				<td width="350">
-				<input name="Nama" type="text" id="Nama" size="40" />
-				</td>
-			</tr>
-			<tr>
-				<td width="120"><b>Alamat</b></td>
-				<td width="350">
-				<input name="Alamat" type="text" id="Alamat" size="40" />
-				</td>
-			</tr>
-			<tr>
-				<td width="120"><b>Kode Pos</b></td>
-				<td width="350">
-				<input name="kd_pos" type="text" id="kd_pos" size="40" />
-				</td>
-			</tr>
-			<tr>
-				<td width="120"><b>No. Telepon</b></td>
-				<td width="350">
-				<input name="No_telp" type="text" id="No_telp" size="40" />
-				</td>
-			</tr>
-			<tr>
-				<td width="120"><b>E-Mail</b></td>
-				<td width="350">
-				<input name="Email" type="Email" id="Email" size="40" />
-				</td>
-			</tr>
-			<?php ?>
-			<tr>
-				<td width="120"><b>Kota</b></td>
-				<td width="350">
-				<select name='id_kota' id='id_kota'>
-					<?php
-					$get_kota=mysql_query('select * from biaya_kirim order by nama_kota');
-					while ($rows=mysql_fetch_array($get_kota)){
-					?>
-					<option value="<?php echo $rows['id_kota']?>"><?php echo $rows['nama_kota']
-						?></option>
-					<?php
-					}//end while
-					?>
-				</select></td>
-			</tr>
-			<tr>
-				<td>&nbsp;</td>
-			
-				<td>
-				<input type="submit" name="tambah" value="Tambah" class="btn" />
-				<input type="reset" name="resetbtn" value="Reset" class="btn btn-danger" />
-				</td>
-			</tr>
-			<tr>
-				<td colspan='2'><div id="form1_errorloc" style="color:red"></div></td>
-			</tr>
-		</table></td>
-	</form>
-	<script language="javaScript" type="text/javascript"
-	xml:space="preserve">
-		//You should create the validator only after the definition of the HTML form
-		var frmvalidator = new Validator("form1");
-		frmvalidator.EnableOnPageErrorDisplaySingleBox();
-		frmvalidator.EnableMsgsTogether();
+	if(isset($_GET['kirim'])){
+		?>
+		<form id="form1" name="form1" method="post" action="pemesan_add.php">
+			<td>
+			<table>
+				<tr>
+					<td width="120"><b>Nama</b></td>
+					<td width="350">
+					<input name="Nama" type="text" id="Nama" size="40" />
+					</td>
+				</tr>
+				<tr>
+					<td width="120"><b>Alamat</b></td>
+					<td width="350">
+					<input name="Alamat" type="text" id="Alamat" size="40" />
+					</td>
+				</tr>
+				<tr>
+					<td width="120"><b>Kode Pos</b></td>
+					<td width="350">
+					<input name="kd_pos" type="text" id="kd_pos" size="40" />
+					</td>
+				</tr>
+				<tr>
+					<td width="120"><b>No. Telepon</b></td>
+					<td width="350">
+					<input name="No_telp" type="text" id="No_telp" size="40" />
+					</td>
+				</tr>
+				<tr>
+					<td width="120"><b>E-Mail</b></td>
+					<td width="350">
+					<input name="Email" type="Email" id="Email" size="40" />
+					</td>
+				</tr>
+				<?php ?>
+				<tr>
+					<td width="120"><b>Kota</b></td>
+					<td width="350">
+					<select name='id_kota' id='id_kota'>
+						<?php
+						$get_kota=mysql_query('select * from biaya_kirim order by nama_kota');
+						while ($rows=mysql_fetch_array($get_kota)){
+						?>
+						<option value="<?php echo $rows['id_kota']?>"><?php echo $rows['nama_kota']
+							?></option>
+						<?php
+						}//end while
+						?>
+					</select></td>
+				</tr>
+				<tr>
+					<td>&nbsp;</td>
+				
+					<td>
+					<input type="submit" name="tambah" value="Tambah" class="btn" />
+					<input type="reset" name="resetbtn" value="Reset" class="btn btn-danger" />
+					</td>
+				</tr>
+				<tr>
+					<td colspan='2'><div id="form1_errorloc" style="color:red"></div></td>
+				</tr>
+			</table></td>
+		</form>
 
-		frmvalidator.addValidation("kd_pemesan", "req", "kode pemesan masih kosong ");
-		frmvalidator.addValidation("Nama", "req", "nama  masih kosong ");
-		frmvalidator.addValidation("Alamat", "req", "alamat masih kosong ");
-		frmvalidator.addValidation("kd_pos", "req", "kode pos masih kosong ");
-		frmvalidator.addValidation("No_telp", "req", "no. telp masih kosong");
-		frmvalidator.addValidation("email", "req", "email masih kosong");
-		frmvalidator.addValidation("kd_pemesan", "alnum_s ", "kode pemesan tidak boleh ada spasi ");
-		frmvalidator.addValidation("Alamat", "minlen=10", "alamat kurang lengkap ");
-		frmvalidator.addValidation("kd_pos", "num", "input harus angka ");
-		frmvalidator.addValidation("No_telp", "num", "input harus angka ");
-		frmvalidator.addValidation("email", "maxlen=50", "maksimal panjang email 50 karakter");
-
-	</script>
 	<?php
-}
+	}
 	?>
 </div>
