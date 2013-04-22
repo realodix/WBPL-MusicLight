c113d408bf3ec62bca80be5bda7a49c1b65e5916
Realodix <realodix@yahoo.com>
2013-04-22 11:16:44 +0700

    Testy: Memindahkan status ke bagian bawah.

--- testimony.php
+++ testimony.php
@@ -2,19 +2,16 @@
 	<h1>Testimony</h2>
 			
 	<?php
-	if (isset($_GET['status'])) {
-		if ($_GET['status'] == 0) {
-			echo "<p id=".'waiting'.">Terima kasih, testimony Anda sudah masuk dan sedang menunggu moderasi dari Admin.</p>";
-		}
-	}
 			
 	$sql="SELECT * FROM  wbpl_testimony where testimony_status='approved' ";
 	$result=mysql_query($sql) or die(mysql_error());
 	while($rows=mysql_fetch_array($result)){
 	?>
 	
+	<br>
 	<p>#<?php echo $rows['kd_testimony'];?> <br>
 	<?php echo $rows['testimony_isi'];?></p>
+	<br>
 	<?php } ?>
 			
 	<hr>
@@ -24,5 +21,12 @@
 	<form name="testimony" method="post" action="admin/wbpl_add-edit.php?action=insert_testimony">
 		<textarea name="testimony_isi" style="width: 550px; height: 160px; resize: none;"></textarea>
 		<br>
+		<?php
+		if (isset($_GET['status'])) {
+			if ($_GET['status'] == 0) {
+				echo "<i id=".'waiting'.">Terima kasih, testimony Anda sudah masuk dan sedang menunggu moderasi dari Admin.</i> <br>";
+			}
+		}
+		?>
 		<input class="btn btn-primary" type="submit" name="testimony_submit" value="Post Testimony"/>
 	</form>
