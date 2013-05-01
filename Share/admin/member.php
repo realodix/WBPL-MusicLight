b15c1233e18d0b6d59d584c5b0c68bf27bf2e4f8
Realodix <realodix@yahoo.com>
2013-05-01 09:36:55 +0700

    Share: Menghapus fitur paging dan menampilkan url yang tidak terlihat.

--- Share/admin/member.php
+++ Share/admin/member.php
@@ -31,30 +31,12 @@ if(isset($_GET['del'])){
 	mysql_query($hapus)or die(mysql_error());
 }
 
-
-$batas = 5;
-if(isset($_GET['halaman'])){
-	$halaman = $_GET['halaman'];
-}
-
-$posisi=null;
-if(empty($halaman)){
-	$posisi=0;
-	$halaman=1;
-}else{
-	$posisi=($halaman-1)* $batas;
-}
-
-$sql=null;
-
 if(isset($_POST['btnCari'])){
 	$cari=$_POST['cari'];
 	//ambil data dari table admin
 	$sql="SELECT * FROM  wbpl_member where username like '%$cari%'";
 }else{
-	$sql="SELECT * FROM  wbpl_member
-			ORDER BY kd_member ASC
-			limit $posisi,$batas";
+	$sql="SELECT * FROM  wbpl_member";
 }
 
 $result=mysql_query($sql) or die(mysql_error());
@@ -68,56 +50,16 @@ while($rows=mysql_fetch_array($result)){
 		<td><a href="index.php?page=profile&id=<?php echo $rows['kd_member']?>"><?php echo $rows['username'];?></a></td>
 		
 		<td>
-			<a class="btn" href="index.php?page=profile&id=<?php echo $rows['kd_member']?>">
-				<i class="icon-edit" title="Edit"></i>
+			<a href="index.php?page=profile&id=<?php echo $rows['kd_member']?>">
+				Edit
 			</a>
-			<a class="btn btn-danger" href="index.php?page=member&del=true&id=<?php echo $rows['kd_member']?>"  onclick="return askUser()";> 
-				<i class="icon-remove" title="Remove"></i>
+			 or
+			<a href="index.php?page=member&del=true&id=<?php echo $rows['kd_member']?>"  onclick="return askUser()";> 
+				Del
 			</a>
 		</td>
 	</tr>
 	<?php
 	}
-
-	//tutup koneksi
 	?>
-	<tr>
-		<td></td>
-		<td align=right colspan='1'><?php
-		if (isset($_GET['status'])) {
-			if ($_GET['status'] == 0) {
-				echo " <div style='color:blue'>Operasi data berhasil</div>";
-			} else {
-				echo "operasi gagal";
-			}
-		}
-		?>
-		</td>
-		<!--<td align=right><a href="index.php?page=pengelola_form_add"> <img src="image/admin/add.jpg"> Add</a></td>-->
-	</tr>
-	<tr></tr>
 </table>
-
-<?php
-
-	//=============CUT HERE====================================
-	$tampil2=mysql_query("select * from wbpl_member");
-	$jmldata=mysql_num_rows($tampil2);
-	$jumlah_halaman=ceil($jmldata/$batas);
-?>
-
-	Jumlah data :<?php echo $jmldata;?>
-	
-	<div class="pagination">
-		<ul>
-		<?php
-		for($i = 1; $i <= $jumlah_halaman; $i++)
-			if($i != $halaman) {
-			echo "<li><a href=index.php?page=member&halaman=$i> $i </a></li>";
-		}else {
-			echo "<li class=".'active'."><a href="."> $i </a></li>";
-		}
-		mysql_close();
-		?>
-		</ul>
-	</div>
